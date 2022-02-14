66f0ce32d1e2e4c9ccfa85bcefc38e9379896510
Daniel Tulpa <dtulpa16@yahoo.com>
2022-02-14 09:58:19 -0600

    Added AWS Lambda Credentials

--- index.js
+++ index.js
@@ -4,15 +4,17 @@ const bodyParser = require('body-parser');
 const app = express()
 const students = require('./routes/student')
 const bot = require('./routes/student')
-
+const serverless = require('serverless-http');
 
 connectDB()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/api/students', students)
+app.use('api/students', students)
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log(`Server started on port: ${port}`)
 })
+// app.listen(3000, () => console.log(`Listening on: 3000`));
+module.exports.handler = serverless(app);
